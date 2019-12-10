0fc4fe8139494fad6cfbe728b3acd5cc80aa0e95
Ale <alearanibar@gmail.com>
2019-12-09 23:28:55 -0800

    first commit

---
+++ seeds/01-users.js
@@ -0,0 +1,15 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('users').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([        
+        { username: 'diandra', website: 'www.diandrassite.com' },
+        { username: 'amy' },
+        { username: 'bill' },
+        { username: 'Carlos' }
+       
+      ]);
+    });
+};
